970cdf22cf232974fc34048650b9d87d777a88bf
Mostafa Rhazlani <mo.rhazlani@gmail.com>
2024-12-17 15:00:20 +0100

    add part header and sidebar

---
+++ resources/views/layout/_SIDEBAR.php
@@ -0,0 +1,29 @@
+<div class="w-full py-3 px-6 md:px-4 fixed bottom-0 md:top-0 z-10 md:w-[90px]">
+    <ul class="flex md:h-full gap-5 md:gap-8 justify-between md:flex-col md:justify-center">
+        <li>
+            <a href="#" class="text-gray-500">
+                <img width="45" src="../../img/home.svg" alt="">
+            </a>
+        </li>
+        <li>
+            <a href="#" class="text-gray-500">
+                <img width="45" src="../../img/videos.svg" alt="">
+            </a>
+        </li>
+        <li>
+            <a href="#" class="text-gray-500">
+                <img class="bg-red-600 rounded-full p-3 hover:bg-red-500" width="45" src="../../img/search.svg" alt="">
+            </a>
+        </li>
+        <li>
+            <a href="#" class="text-gray-500">
+                <img width="45" src="../../img/envelope-solid.svg" alt="">
+            </a>
+        </li>
+        <li>
+            <a href="#" class="text-gray-500">
+                <img width="45" src="../../img/account.svg" alt="">
+            </a>
+        </li>
+    </ul>
+</div>
