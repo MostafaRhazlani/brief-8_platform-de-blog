9b312b0a13de8dc4811d78bf739f3c2fecc22784
Mostafa Rhazlani <mo.rhazlani@gmail.com>
2024-12-18 00:16:06 +0100

    fix header and sidebar

--- resources/views/layout/_HEAD.php
+++ resources/views/layout/_HEAD.php
@@ -10,15 +10,16 @@
     <link href="https://fonts.googleapis.com/css2?family=Edu+AU+VIC+WA+NT+Arrows:wght@400..700&family=Edu+AU+VIC+WA+NT+Pre:wght@400..700&family=League+Spartan:wght@100..900&family=Oswald:wght@200..700&family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&family=Sansita+Swashed:wght@300..900&display=swap" rel="stylesheet">
     <script src="https://cdn.tailwindcss.com"></script>
 </head>
-<body class="font-['poppins]">
+<body class="font-[poppins]">
 
 <div class="relative h-screen">
-    <div class="p-3 md:pl-28">
+    <div class="p-3 md:pl-28 fixed top-0 z-20 bg-white w-full shadow-[0px_0px_5px_1px_#c2c2c2]">
         <div class="flex items-center gap-6">
             <img width="60" class="rounded-full" src="../../img/photo youcode.jpg" alt="">
 
-            <span class="text-4xl">
+            <a href="#" class="text-2xl">
                 <i class="fa-regular fa-bell"></i>
-            </span>
+            </a>
+            <div class="bg-[#c2c2c2]"></div>
         </div>
     </div>
