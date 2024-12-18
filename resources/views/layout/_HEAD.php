657a7ec23ed9f3dcf715dec2d2223fa5fbd723b7
Mostafa Rhazlani <mo.rhazlani@gmail.com>
2024-12-18 11:20:19 +0100

    include file js and css

--- resources/views/layout/_HEAD.php
+++ resources/views/layout/_HEAD.php
@@ -9,6 +9,7 @@
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Edu+AU+VIC+WA+NT+Arrows:wght@400..700&family=Edu+AU+VIC+WA+NT+Pre:wght@400..700&family=League+Spartan:wght@100..900&family=Oswald:wght@200..700&family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&family=Sansita+Swashed:wght@300..900&display=swap" rel="stylesheet">
     <script src="https://cdn.tailwindcss.com"></script>
+    <link rel="stylesheet" href="../../css/style.css">
 </head>
 <body class="font-[poppins]">
 
