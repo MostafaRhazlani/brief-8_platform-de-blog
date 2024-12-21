61dd9040bba84e7cb287a4caeb0a94e53c6b787c
Mostafa Rhazlani <mo.rhazlani@gmail.com>
2024-12-21 16:24:03 +0100

    fix some paths in file head

--- resources/views/layout/_HEAD.php
+++ resources/views/layout/_HEAD.php
@@ -9,7 +9,7 @@
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Edu+AU+VIC+WA+NT+Arrows:wght@400..700&family=Edu+AU+VIC+WA+NT+Pre:wght@400..700&family=League+Spartan:wght@100..900&family=Oswald:wght@200..700&family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&family=Sansita+Swashed:wght@300..900&display=swap" rel="stylesheet">
     <script src="https://cdn.tailwindcss.com"></script>
-    <link rel="stylesheet" href="../../css/style.css">
+    <link rel="stylesheet" href="/resources/css/style.css">
 </head>
 <body class="font-[Roboto]">
 
@@ -30,7 +30,7 @@
         
                         <div class="popupUser hidden absolute mt-2 w-32 bg-white shadow-[0px_0px_5px_1px_#c2c2c2] p-2 rounded-md">
                             <a href class="flex items-center p-1 hover:bg-gray-200 cursor-pointer rounded-sm"><i class="fa-solid fa-user"></i>&nbsp;Profile</a>
-                            <a href="../auth/logout.php" class="flex items-center p-1 hover:bg-gray-200 cursor-pointer rounded-sm"><i class="fa-solid fa-right-from-bracket"></i>&nbsp;Logout</a>
+                            <a href="/resources/views/auth/logout.php" class="flex items-center p-1 hover:bg-gray-200 cursor-pointer rounded-sm"><i class="fa-solid fa-right-from-bracket"></i>&nbsp;Logout</a>
                         </div>
                     </div>
         
@@ -38,13 +38,11 @@
                         <i class="fa-regular fa-bell"></i>
                     </a>
                 </div>
-            <?php } ?>
-
-            <?php if(!isset($_SESSION['user'])) { ?>
+            <?php }else { ?>
                 <div class="w-full flex justify-end">
                     <div class="flex gap-3">
-                        <a href="../auth/login.php" class="bg-red-600 text-white py-1 px-4 rounded-md hover:bg-red-500">Sign in</a>
-                        <a href="../auth/register.php" class="bg-[#200E32] text-white py-1 px-4 rounded-md hover:bg-[#5f2f90]">Sign up</a>
+                        <a href="/resources/views/auth/login.php" class="bg-red-600 text-white py-1 px-4 rounded-md hover:bg-red-500">Sign in</a>
+                        <a href="/resources/views/auth/register.php" class="bg-[#200E32] text-white py-1 px-4 rounded-md hover:bg-[#5f2f90]">Sign up</a>
                     </div>
                 </div>
             <?php } ?>
