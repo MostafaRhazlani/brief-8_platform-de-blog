414f2ec426d89b7303d5cbe5d895e9ef9aad315f
Mostafa Rhazlani <mo.rhazlani@gmail.com>
2024-12-21 19:47:36 +0100

    show all coments of article

--- resources/views/blog/detailsArticle.php
+++ resources/views/blog/detailsArticle.php
@@ -5,9 +5,11 @@
     $idArticle = isset($_GET['idArticle']) ? $_GET['idArticle'] : 0;
 
     $getCategories = mysqli_query($conn, "SELECT * FROM categories");
-    $getArticles = mysqli_query($conn,"SELECT articles.*, users.username, users.email FROM articles inner join users on users.id = articles.idUser WHERE articles.id = $idArticle");
 
+    $getArticles = mysqli_query($conn,"SELECT articles.*, users.username, users.email FROM articles inner join users on users.id = articles.idUser WHERE articles.id = $idArticle");
     $resultArticle = mysqli_fetch_assoc($getArticles);
+
+    $getComments = mysqli_query($conn, "SELECT comments.*, email, username FROM comments JOIN users ON users.id = comments.idUser WHERE idArticle = $idArticle");
 ?>
 
 <?php include('../layout/_HEAD.php') ?>
@@ -65,9 +67,9 @@
                 <div class="flex justify-between border-b-2 border-t-2 py-2">
                     <div class="flex gap-5">
                         <div class="flex items-center">
-                            <a href="#" class="mr-1">
+                            <span class="mr-1">
                                 <svg xmlns="http://www.w3.org/2000/svg" width="20px" height="20px" fill="#cf4c4c" viewBox="0 0 512 512"><!--!Font Awesome Free 6.7.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.--><path d="M47.6 300.4L228.3 469.1c7.5 7 17.4 10.9 27.7 10.9s20.2-3.9 27.7-10.9L464.4 300.4c30.4-28.3 47.6-68 47.6-109.5v-5.8c0-69.9-50.5-129.5-119.4-141C347 36.5 300.6 51.4 268 84L256 96 244 84c-32.6-32.6-79-47.5-124.6-39.9C50.5 55.6 0 115.2 0 185.1v5.8c0 41.5 17.2 81.2 47.6 109.5z"/></svg>
-                            </a>
+                            </span>
                             <!-- count total likes -->
                             <?php
                                 $getAllLikes = mysqli_query($conn, "SELECT * FROM likes WHERE idArticle = $idArticle");
@@ -77,10 +79,14 @@
                         </div>
                         <div class="flex items-center">
                             <!-- icon comments -->
-                            <a href="#" class="mr-1">
+                            <span class="mr-1">
                                 <svg xmlns="http://www.w3.org/2000/svg" width="20px" height="20px" viewBox="0 -960 960 960" fill="#6d6972"><path d="M880-80 720-240H320q-33 0-56.5-23.5T240-320v-40h440q33 0 56.5-23.5T760-440v-280h40q33 0 56.5 23.5T880-640v560ZM160-473l47-47h393v-280H160v327ZM80-280v-520q0-33 23.5-56.5T160-880h440q33 0 56.5 23.5T680-800v280q0 33-23.5 56.5T600-440H240L80-280Zm80-240v-280 280Z"/></svg>
-                            </a>
-                            0 comment
+                            </span>
+                            <?php
+                                $getAllComments = mysqli_query($conn, "SELECT * FROM comments WHERE idArticle = $idArticle");
+                                $totalComments = mysqli_num_rows($getAllComments);
+                                echo "$totalComments comment";
+                            ?>
                         </div>
                     </div>
                     <!-- icon views -->
@@ -112,17 +118,9 @@
                                     </button>
                                 <?php } ?>
                             </form>
-                            <!-- icon comments for users -->
-                            <a href="./detailsArticle.php?idArticle=<?php echo $resultArticle['id'] ?>" class="hover:scale-110 transition duration-300 ease-in-out cursor-pointer">
-                                <svg xmlns="http://www.w3.org/2000/svg" width="30px" height="30px" viewBox="0 -960 960 960" fill="#6d6972"><path d="M880-80 720-240H320q-33 0-56.5-23.5T240-320v-40h440q33 0 56.5-23.5T760-440v-280h40q33 0 56.5 23.5T880-640v560ZM160-473l47-47h393v-280H160v327ZM80-280v-520q0-33 23.5-56.5T160-880h440q33 0 56.5 23.5T680-800v280q0 33-23.5 56.5T600-440H240L80-280Zm80-240v-280 280Z"/></svg>
-                            </a>
                         <?php } else { ?>
                             <a href="../auth/login.php" class="hover:scale-110 transition duration-300 ease-in-out cursor-pointer">
                                 <svg xmlns="http://www.w3.org/2000/svg" width="28px" height="28px" fill="#cf4c4c" viewBox="0 0 512 512"><!--!Font Awesome Free 6.7.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.--><path d="M225.8 468.2l-2.5-2.3L48.1 303.2C17.4 274.7 0 234.7 0 192.8l0-3.3c0-70.4 50-130.8 119.2-144C158.6 37.9 198.9 47 231 69.6c9 6.4 17.4 13.8 25 22.3c4.2-4.8 8.7-9.2 13.5-13.3c3.7-3.2 7.5-6.2 11.5-9c0 0 0 0 0 0C313.1 47 353.4 37.9 392.8 45.4C462 58.6 512 119.1 512 189.5l0 3.3c0 41.9-17.4 81.9-48.1 110.4L288.7 465.9l-2.5 2.3c-8.2 7.6-19 11.9-30.2 11.9s-22-4.2-30.2-11.9zM239.1 145c-.4-.3-.7-.7-1-1.1l-17.8-20-.1-.1s0 0 0 0c-23.1-25.9-58-37.7-92-31.2C81.6 101.5 48 142.1 48 189.5l0 3.3c0 28.5 11.9 55.8 32.8 75.2L256 430.7 431.2 268c20.9-19.4 32.8-46.7 32.8-75.2l0-3.3c0-47.3-33.6-88-80.1-96.9c-34-6.5-69 5.4-92 31.2c0 0 0 0-.1 .1s0 0-.1 .1l-17.8 20c-.3 .4-.7 .7-1 1.1c-4.5 4.5-10.6 7-16.9 7s-12.4-2.5-16.9-7z"/></svg>
-                            </a>
-                                <!-- icon comments for visitor -->
-                            <a href="../auth/login.php" class="hover:scale-110 transition duration-300 ease-in-out cursor-pointer">
-                                <svg xmlns="http://www.w3.org/2000/svg" width="30px" height="30px" viewBox="0 -960 960 960" fill="#6d6972"><path d="M880-80 720-240H320q-33 0-56.5-23.5T240-320v-40h440q33 0 56.5-23.5T760-440v-280h40q33 0 56.5 23.5T880-640v560ZM160-473l47-47h393v-280H160v327ZM80-280v-520q0-33 23.5-56.5T160-880h440q33 0 56.5 23.5T680-800v280q0 33-23.5 56.5T600-440H240L80-280Zm80-240v-280 280Z"/></svg>
                             </a>
                         <?php } ?>
                     </div>
@@ -135,102 +133,34 @@
                 </div>
                 <div class="rounded-md bg-gray-200 h-96 p-3 mb-16 md:mb-0 flex flex-col justify-between">
                     <div class="overflow-scroll hideScrollbar mb-2">
-                        <div class="flex flex-col mb-5">
-                            <div class="w-full flex">
-                                <div class="mr-2 mt-1">
-                                    <img width="40" src="../../img/149071.png" alt="">
-                                </div>
-                                <div class="p-2 rounded-lg bg-white">
-                                    <div class="text-[14px] mb-2">
-                                        <p>username</p>
+                        <?php if($getComments) { ?>
+                            <?php while($comment = mysqli_fetch_assoc($getComments)) { ?>
+                                <div class="flex flex-col mb-5">
+                                    <div class="w-full flex">
+                                        <div class="mr-2 mt-1">
+                                            <img width="40" src="../../img/149071.png" alt="">
+                                        </div>
+                                        <div class="p-2 rounded-lg <?php echo ($comment['idUser'] == $_SESSION['user']['id']) ? 'bg-[#553674] text-white' : 'bg-white' ?>">
+                                            <div class="text-[14px] mb-2">
+                                                <p><?php echo $comment['username'] ?></p>
+                                            </div>
+                                            <div>
+                                                <p><?php echo $comment['content'] ?></p>
+                                            </div>
+                                        </div>
                                     </div>
-                                    <div>
-                                        <p>keep going don't give up my friend dlko mpod,c ceok ddod njur neijef nefiej</p>
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="ml-14 flex text-[12px] gap-3 mt-1 text-gray-600">
-                                <p>2h ago</p>
-                                <form class="flex items-center gap-1" action="./add_delete_like.php" method="post">
-                                    <button class="hover:scale-110 transition duration-300 ease-in-out">
-                                        <svg xmlns="http://www.w3.org/2000/svg" width="16px" height="16px" fill="#cf4c4c" viewBox="0 0 512 512"><!--!Font Awesome Free 6.7.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.--><path d="M47.6 300.4L228.3 469.1c7.5 7 17.4 10.9 27.7 10.9s20.2-3.9 27.7-10.9L464.4 300.4c30.4-28.3 47.6-68 47.6-109.5v-5.8c0-69.9-50.5-129.5-119.4-141C347 36.5 300.6 51.4 268 84L256 96 244 84c-32.6-32.6-79-47.5-124.6-39.9C50.5 55.6 0 115.2 0 185.1v5.8c0 41.5 17.2 81.2 47.6 109.5z"/></svg>
-                                    </button>
-                                    <p>like</p>
-                                </form>
-                            </div>
-                        </div>
-                        <div class="flex flex-col mb-5">
-                            <div class="w-full flex">
-                                <div class="mr-2 mt-1">
-                                    <img width="40" src="../../img/149071.png" alt="">
-                                </div>
-                                <div class="p-2 rounded-lg bg-white">
-                                    <div class="text-[14px] mb-2">
-                                        <p>username</p>
-                                    </div>
-                                    <div>
-                                        <p>keep going don't give up my friend dlko mpod,c ceok ddod njur neijef nefiej</p>
+                                    <div class="ml-14 flex text-[12px] gap-3 mt-1 text-gray-600">
+                                        <p>2h ago</p>
+                                        <form class="flex items-center gap-1" action="./add_delete_like.php" method="post">
+                                            <button class="hover:scale-110 transition duration-300 ease-in-out">
+                                                <svg xmlns="http://www.w3.org/2000/svg" width="16px" height="16px" fill="#cf4c4c" viewBox="0 0 512 512"><!--!Font Awesome Free 6.7.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.--><path d="M47.6 300.4L228.3 469.1c7.5 7 17.4 10.9 27.7 10.9s20.2-3.9 27.7-10.9L464.4 300.4c30.4-28.3 47.6-68 47.6-109.5v-5.8c0-69.9-50.5-129.5-119.4-141C347 36.5 300.6 51.4 268 84L256 96 244 84c-32.6-32.6-79-47.5-124.6-39.9C50.5 55.6 0 115.2 0 185.1v5.8c0 41.5 17.2 81.2 47.6 109.5z"/></svg>
+                                            </button>
+                                            <p>like</p>
+                                        </form>
                                     </div>
                                 </div>
-                            </div>
-                            <div class="ml-14 flex text-[12px] gap-3 mt-1 text-gray-600">
-                                <p>2h ago</p>
-                                <form class="flex items-center gap-1" action="./add_delete_like.php" method="post">
-                                    <button class="hover:scale-110 transition duration-300 ease-in-out">
-                                        <svg xmlns="http://www.w3.org/2000/svg" width="16px" height="16px" fill="#cf4c4c" viewBox="0 0 512 512"><!--!Font Awesome Free 6.7.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.--><path d="M47.6 300.4L228.3 469.1c7.5 7 17.4 10.9 27.7 10.9s20.2-3.9 27.7-10.9L464.4 300.4c30.4-28.3 47.6-68 47.6-109.5v-5.8c0-69.9-50.5-129.5-119.4-141C347 36.5 300.6 51.4 268 84L256 96 244 84c-32.6-32.6-79-47.5-124.6-39.9C50.5 55.6 0 115.2 0 185.1v5.8c0 41.5 17.2 81.2 47.6 109.5z"/></svg>
-                                    </button>
-                                    <p>like</p>
-                                </form>
-                            </div>
-                        </div>
-                        <div class="flex flex-col mb-5">
-                            <div class="w-full flex">
-                                <div class="mr-2 mt-1">
-                                    <img width="40" src="../../img/149071.png" alt="">
-                                </div>
-                                <div class="p-2 rounded-lg bg-[#553674] text-white">
-                                    <div class="text-[14px] mb-2">
-                                        <p>username</p>
-                                    </div>
-                                    <div>
-                                        <p>keep going don't give up my friend dlko mpod,c ceok ddod njur neijef nefiej</p>
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="ml-14 flex text-[12px] gap-3 mt-1 text-gray-600">
-                                <p>2h ago</p>
-                                <form class="flex items-center gap-1" action="./add_delete_like.php" method="post">
-                                    <button class="hover:scale-110 transition duration-300 ease-in-out">
-                                        <svg xmlns="http://www.w3.org/2000/svg" width="16px" height="16px" fill="#cf4c4c" viewBox="0 0 512 512"><!--!Font Awesome Free 6.7.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.--><path d="M47.6 300.4L228.3 469.1c7.5 7 17.4 10.9 27.7 10.9s20.2-3.9 27.7-10.9L464.4 300.4c30.4-28.3 47.6-68 47.6-109.5v-5.8c0-69.9-50.5-129.5-119.4-141C347 36.5 300.6 51.4 268 84L256 96 244 84c-32.6-32.6-79-47.5-124.6-39.9C50.5 55.6 0 115.2 0 185.1v5.8c0 41.5 17.2 81.2 47.6 109.5z"/></svg>
-                                    </button>
-                                    <p>like</p>
-                                </form>
-                            </div>
-                        </div>
-                        <div class="flex flex-col mb-5">
-                            <div class="w-full flex">
-                                <div class="mr-2 mt-1">
-                                    <img width="40" src="../../img/149071.png" alt="">
-                                </div>
-                                <div class="p-2 rounded-lg bg-white">
-                                    <div class="text-[14px] mb-2">
-                                        <p>username</p>
-                                    </div>
-                                    <div>
-                                        <p>keep going don't give up my friend dlko mpod,c ceok ddod njur neijef nefiej</p>
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="ml-14 flex text-[12px] gap-3 mt-1 text-gray-600">
-                                <p>2h ago</p>
-                                <form class="flex items-center gap-1" action="./add_delete_like.php" method="post">
-                                    <button class="hover:scale-110 transition duration-300 ease-in-out">
-                                        <svg xmlns="http://www.w3.org/2000/svg" width="16px" height="16px" fill="#cf4c4c" viewBox="0 0 512 512"><!--!Font Awesome Free 6.7.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.--><path d="M47.6 300.4L228.3 469.1c7.5 7 17.4 10.9 27.7 10.9s20.2-3.9 27.7-10.9L464.4 300.4c30.4-28.3 47.6-68 47.6-109.5v-5.8c0-69.9-50.5-129.5-119.4-141C347 36.5 300.6 51.4 268 84L256 96 244 84c-32.6-32.6-79-47.5-124.6-39.9C50.5 55.6 0 115.2 0 185.1v5.8c0 41.5 17.2 81.2 47.6 109.5z"/></svg>
-                                    </button>
-                                    <p>like</p>
-                                </form>
-                            </div>
-                        </div>
+                            <?php } ?>
+                        <?php } ?>
                     </div>
                     <form action="" class="w-full">
                         <div class="flex flex-col items-end rounded-md overflow-hidden bg-gray-300">
