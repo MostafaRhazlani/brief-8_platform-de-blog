10ca6ea9303641df23fd6d51d45e449e2b13b5d4
Mostafa Rhazlani <mo.rhazlani@gmail.com>
2024-12-21 19:11:47 +0100

    fix all buttons like, comment, views

--- resources/views/blog/blog.php
+++ resources/views/blog/blog.php
@@ -13,12 +13,12 @@
 <div class="w-full md:w-4/6 lg:w-[40%] md:mx-auto pt-24">
     <div class="w-full bg-white shadow-[0px_0px_2px_#9b9b9b] rounded-lg">
         <?php if(isset($_SESSION['user'])) { ?>
-        <div class="w-full p-4">
-            <div class="showFormArticle py-3 px-5 rounded-md bg-gray-200 text-gray-500 font-medium cursor-pointer hover:bg-gray-300 flex items-center gap-2">
-                <i class="fa-solid fa-newspaper"></i>
-                <h1>create article</h1>
+            <div class="w-full p-4">
+                <div class="showFormArticle py-3 px-5 rounded-md bg-gray-200 text-gray-500 font-medium cursor-pointer hover:bg-gray-300 flex items-center gap-2">
+                    <i class="fa-solid fa-newspaper"></i>
+                    <h1>create article</h1>
+                </div>
             </div>
-        </div>
         <?php } ?>
         <?php if($getArticles) { ?>
             <?php while($article = mysqli_fetch_assoc($getArticles)) { ?>
@@ -70,42 +70,55 @@
                             <?php } ?>
                         </div>
                         <div class="mb-4">
-                            <h1 class="font-semibold text-3xl mb-2"><?php echo $article['title'] ?></h1>
+                            <h1 class="font-semibold text-3xl mb-2">
+                                    <a href="./detailsArticle.php?idArticle=<?php echo $article['id'] ?>" class="hover:text-red-300">
+                                        <?php echo $article['title'] ?>
+                                    </a>
+                                </h1>
                             <p class="break-words">
                                 <?php echo $article['content'] ?>
                             </p>
                         </div>
                         <div class="flex justify-between border-b-2 border-t-2 py-2">
-                            <div class="flex items-center">
-                                <a href="#" class="mr-1">
-                                    <svg xmlns="http://www.w3.org/2000/svg" width="24px" height="24px" fill="#cf4c4c" viewBox="0 0 512 512"><!--!Font Awesome Free 6.7.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.--><path d="M47.6 300.4L228.3 469.1c7.5 7 17.4 10.9 27.7 10.9s20.2-3.9 27.7-10.9L464.4 300.4c30.4-28.3 47.6-68 47.6-109.5v-5.8c0-69.9-50.5-129.5-119.4-141C347 36.5 300.6 51.4 268 84L256 96 244 84c-32.6-32.6-79-47.5-124.6-39.9C50.5 55.6 0 115.2 0 185.1v5.8c0 41.5 17.2 81.2 47.6 109.5z"/></svg>
-                                </a>
-                                <!-- count total likes -->
-                                <?php
-                                    $getAllLikes = mysqli_query($conn, "SELECT * FROM likes WHERE idArticle = $idArticle");
-                                    $totalLikes = mysqli_num_rows($getAllLikes);
-                                    echo "$totalLikes like";
-                                ?>
+                            <div class="flex gap-5">
+                                <div class="flex items-center">
+                                    <a href="#" class="mr-1">
+                                        <svg xmlns="http://www.w3.org/2000/svg" width="20px" height="20px" fill="#cf4c4c" viewBox="0 0 512 512"><!--!Font Awesome Free 6.7.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.--><path d="M47.6 300.4L228.3 469.1c7.5 7 17.4 10.9 27.7 10.9s20.2-3.9 27.7-10.9L464.4 300.4c30.4-28.3 47.6-68 47.6-109.5v-5.8c0-69.9-50.5-129.5-119.4-141C347 36.5 300.6 51.4 268 84L256 96 244 84c-32.6-32.6-79-47.5-124.6-39.9C50.5 55.6 0 115.2 0 185.1v5.8c0 41.5 17.2 81.2 47.6 109.5z"/></svg>
+                                    </a>
+                                    <!-- count total likes -->
+                                    <?php
+                                        $getAllLikes = mysqli_query($conn, "SELECT * FROM likes WHERE idArticle = $idArticle");
+                                        $totalLikes = mysqli_num_rows($getAllLikes);
+                                        echo "$totalLikes like";
+                                    ?>
+                                </div>
+                                <div class="flex items-center">
+                                    <!-- icon comments -->
+                                    <a href="#" class="mr-1">
+                                        <svg xmlns="http://www.w3.org/2000/svg" width="20px" height="20px" viewBox="0 -960 960 960" fill="#6d6972"><path d="M880-80 720-240H320q-33 0-56.5-23.5T240-320v-40h440q33 0 56.5-23.5T760-440v-280h40q33 0 56.5 23.5T880-640v560ZM160-473l47-47h393v-280H160v327ZM80-280v-520q0-33 23.5-56.5T160-880h440q33 0 56.5 23.5T680-800v280q0 33-23.5 56.5T600-440H240L80-280Zm80-240v-280 280Z"/></svg>
+                                    </a>
+                                    0 comment
+                                </div>
                             </div>
                             <!-- icon views -->
                             <div class="flex items-center">
                                 <a href="#" class="mr-1">
-                                    <svg xmlns="http://www.w3.org/2000/svg" class="hover:fill-[#200E32] transition duration-300 ease-in-out" height="24px" viewBox="0 -960 960 960" width="24px" fill="#6d6972"><path d="M480-320q75 0 127.5-52.5T660-500q0-75-52.5-127.5T480-680q-75 0-127.5 52.5T300-500q0 75 52.5 127.5T480-320Zm0-72q-45 0-76.5-31.5T372-500q0-45 31.5-76.5T480-608q45 0 76.5 31.5T588-500q0 45-31.5 76.5T480-392Zm0 192q-146 0-266-81.5T40-500q54-137 174-218.5T480-800q146 0 266 81.5T920-500q-54 137-174 218.5T480-200Zm0-300Zm0 220q113 0 207.5-59.5T832-500q-50-101-144.5-160.5T480-720q-113 0-207.5 59.5T128-500q50 101 144.5 160.5T480-280Z"/></svg>
+                                    <svg xmlns="http://www.w3.org/2000/svg" class="hover:fill-[#200E32] transition duration-300 ease-in-out" height="20px" viewBox="0 -960 960 960" width="20px" fill="#6d6972"><path d="M480-320q75 0 127.5-52.5T660-500q0-75-52.5-127.5T480-680q-75 0-127.5 52.5T300-500q0 75 52.5 127.5T480-320Zm0-72q-45 0-76.5-31.5T372-500q0-45 31.5-76.5T480-608q45 0 76.5 31.5T588-500q0 45-31.5 76.5T480-392Zm0 192q-146 0-266-81.5T40-500q54-137 174-218.5T480-800q146 0 266 81.5T920-500q-54 137-174 218.5T480-200Zm0-300Zm0 220q113 0 207.5-59.5T832-500q-50-101-144.5-160.5T480-720q-113 0-207.5 59.5T128-500q50 101 144.5 160.5T480-280Z"/></svg>
                                 </a>
                                 <p>0 views</p>
                             </div>
                         </div>
                         <div class="flex justify-between py-2 mb-6">
                             <div class="flex gap-3">
-
                                 <!-- icon like -->
-                                    <?php $idUser = isset($_SESSION['user']) ? $_SESSION['user']['id'] : 0;
-                                          $getLikes = mysqli_query($conn, "SELECT * FROM likes WHERE idUser = $idUser AND idArticle = $idArticle");
-                                          $userLike = mysqli_num_rows($getLikes); 
-                                    ?>
-                                    <?php if(isset($_SESSION['user'])) { ?>
+                                <?php $idUser = isset($_SESSION['user']) ? $_SESSION['user']['id'] : 0;
+                                        $getLikes = mysqli_query($conn, "SELECT * FROM likes WHERE idUser = $idUser AND idArticle = $idArticle");
+                                        $userLike = mysqli_num_rows($getLikes); 
+                                ?>
+                                <?php if(isset($_SESSION['user'])) { ?>
                                     <form action="./add_delete_like.php" method="post">
-                                        <input type="hidden" value="<?php echo $article['id'] ?>" name="idArticle" id="">
+                                        <input type="hidden" value="<?php echo $article['id'] ?>" name="idArticle">
+                                        <input type="hidden" value="<?php echo $_SERVER['PHP_SELF'] ?>" name="currentPath">
                                         <?php if($userLike == 0) { ?>
                                             <button class="hover:scale-110 transition duration-300 ease-in-out">
                                             <svg xmlns="http://www.w3.org/2000/svg" width="28px" height="28px" fill="#cf4c4c" viewBox="0 0 512 512"><!--!Font Awesome Free 6.7.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.--><path d="M225.8 468.2l-2.5-2.3L48.1 303.2C17.4 274.7 0 234.7 0 192.8l0-3.3c0-70.4 50-130.8 119.2-144C158.6 37.9 198.9 47 231 69.6c9 6.4 17.4 13.8 25 22.3c4.2-4.8 8.7-9.2 13.5-13.3c3.7-3.2 7.5-6.2 11.5-9c0 0 0 0 0 0C313.1 47 353.4 37.9 392.8 45.4C462 58.6 512 119.1 512 189.5l0 3.3c0 41.9-17.4 81.9-48.1 110.4L288.7 465.9l-2.5 2.3c-8.2 7.6-19 11.9-30.2 11.9s-22-4.2-30.2-11.9zM239.1 145c-.4-.3-.7-.7-1-1.1l-17.8-20-.1-.1s0 0 0 0c-23.1-25.9-58-37.7-92-31.2C81.6 101.5 48 142.1 48 189.5l0 3.3c0 28.5 11.9 55.8 32.8 75.2L256 430.7 431.2 268c20.9-19.4 32.8-46.7 32.8-75.2l0-3.3c0-47.3-33.6-88-80.1-96.9c-34-6.5-69 5.4-92 31.2c0 0 0 0-.1 .1s0 0-.1 .1l-17.8 20c-.3 .4-.7 .7-1 1.1c-4.5 4.5-10.6 7-16.9 7s-12.4-2.5-16.9-7z"/></svg>
@@ -116,15 +129,19 @@
                                             </button>
                                         <?php } ?>
                                     </form>
+                                    <!-- icon comments for users -->
+                                    <a href="./detailsArticle.php?idArticle=<?php echo $article['id'] ?>" class="hover:scale-110 transition duration-300 ease-in-out cursor-pointer">
+                                        <svg xmlns="http://www.w3.org/2000/svg" width="30px" height="30px" viewBox="0 -960 960 960" fill="#6d6972"><path d="M880-80 720-240H320q-33 0-56.5-23.5T240-320v-40h440q33 0 56.5-23.5T760-440v-280h40q33 0 56.5 23.5T880-640v560ZM160-473l47-47h393v-280H160v327ZM80-280v-520q0-33 23.5-56.5T160-880h440q33 0 56.5 23.5T680-800v280q0 33-23.5 56.5T600-440H240L80-280Zm80-240v-280 280Z"/></svg>
+                                    </a>
                                 <?php } else { ?>
                                     <a href="../auth/login.php" class="hover:scale-110 transition duration-300 ease-in-out cursor-pointer">
                                         <svg xmlns="http://www.w3.org/2000/svg" width="28px" height="28px" fill="#cf4c4c" viewBox="0 0 512 512"><!--!Font Awesome Free 6.7.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.--><path d="M225.8 468.2l-2.5-2.3L48.1 303.2C17.4 274.7 0 234.7 0 192.8l0-3.3c0-70.4 50-130.8 119.2-144C158.6 37.9 198.9 47 231 69.6c9 6.4 17.4 13.8 25 22.3c4.2-4.8 8.7-9.2 13.5-13.3c3.7-3.2 7.5-6.2 11.5-9c0 0 0 0 0 0C313.1 47 353.4 37.9 392.8 45.4C462 58.6 512 119.1 512 189.5l0 3.3c0 41.9-17.4 81.9-48.1 110.4L288.7 465.9l-2.5 2.3c-8.2 7.6-19 11.9-30.2 11.9s-22-4.2-30.2-11.9zM239.1 145c-.4-.3-.7-.7-1-1.1l-17.8-20-.1-.1s0 0 0 0c-23.1-25.9-58-37.7-92-31.2C81.6 101.5 48 142.1 48 189.5l0 3.3c0 28.5 11.9 55.8 32.8 75.2L256 430.7 431.2 268c20.9-19.4 32.8-46.7 32.8-75.2l0-3.3c0-47.3-33.6-88-80.1-96.9c-34-6.5-69 5.4-92 31.2c0 0 0 0-.1 .1s0 0-.1 .1l-17.8 20c-.3 .4-.7 .7-1 1.1c-4.5 4.5-10.6 7-16.9 7s-12.4-2.5-16.9-7z"/></svg>
                                     </a>
+                                        <!-- icon comments for visitor -->
+                                    <a href="../auth/login.php" class="hover:scale-110 transition duration-300 ease-in-out cursor-pointer">
+                                        <svg xmlns="http://www.w3.org/2000/svg" width="30px" height="30px" viewBox="0 -960 960 960" fill="#6d6972"><path d="M880-80 720-240H320q-33 0-56.5-23.5T240-320v-40h440q33 0 56.5-23.5T760-440v-280h40q33 0 56.5 23.5T880-640v560ZM160-473l47-47h393v-280H160v327ZM80-280v-520q0-33 23.5-56.5T160-880h440q33 0 56.5 23.5T680-800v280q0 33-23.5 56.5T600-440H240L80-280Zm80-240v-280 280Z"/></svg>
+                                    </a>
                                 <?php } ?>
-                                <!-- icon comments -->
-                                <a href="#" class="hover:scale-110 transition duration-300 ease-in-out cursor-pointer">
-                                    <svg xmlns="http://www.w3.org/2000/svg" width="30px" height="30px" viewBox="0 -960 960 960" fill="#6d6972"><path d="M880-80 720-240H320q-33 0-56.5-23.5T240-320v-40h440q33 0 56.5-23.5T760-440v-280h40q33 0 56.5 23.5T880-640v560ZM160-473l47-47h393v-280H160v327ZM80-280v-520q0-33 23.5-56.5T160-880h440q33 0 56.5 23.5T680-800v280q0 33-23.5 56.5T600-440H240L80-280Zm80-240v-280 280Z"/></svg>
-                                </a>
                             </div>
                             <div class="flex">
                                 <!-- icons saves -->
