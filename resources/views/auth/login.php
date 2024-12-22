31b60de3e677aba5b57b04974a356abf64edbeb7
Mostafa Rhazlani <mo.rhazlani@gmail.com>
2024-12-22 21:04:28 +0100

    change font familty

--- resources/views/auth/login.php
+++ resources/views/auth/login.php
@@ -15,8 +15,9 @@
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Edu+AU+VIC+WA+NT+Arrows:wght@400..700&family=Edu+AU+VIC+WA+NT+Pre:wght@400..700&family=League+Spartan:wght@100..900&family=Oswald:wght@200..700&family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&family=Sansita+Swashed:wght@300..900&display=swap" rel="stylesheet">
     <script src="https://cdn.tailwindcss.com"></script>
+    <link rel="stylesheet" href="/resources/css/style.css">
 </head>
-<body class="font-[Poppins]">
+<body>
     <div class="h-screen w-full">
         <div class="w-5/6 md:w-3/5 lg:w-2/6 mx-auto mt-40 bg-white shadow-[0px_0px_4px_#a5a4a4] rounded-md p-4">
             <h1 class="text-4xl text-center mb-10">Sgin in</h1>
